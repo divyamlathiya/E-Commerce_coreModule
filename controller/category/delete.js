9d90a3478f2cefee24846b78191db416173806db
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-05 15:18:01 +0530

    Update deleteProduct to use categoryName from request

--- controller/category/delete.js
+++ controller/category/delete.js
@@ -5,18 +5,24 @@ var categoryRegister = require('../../models/category.model.js');
 
 /* GET users listing. */
 async function deleteProduct(req, res, next) {
-  
-  const categoryData = await categoryRegister.findOne({ categoryName: 'Home&Kitchen' }).lean();
 
-  if (categoryData) {
-    const deleteCategory = await categoryRegister.findOneAndDelete({ categoryName: 'Home&Kitchen' }).lean();
-    if (deleteCategory) {
-      response.onSuccess(res, deleteCategory, 'category is deleted');
+  const { categoryName } = req.body;
+  
+  const categoryData = await categoryRegister.findOne({ categoryName }).lean();
+
+  if (categoryName) {
+    if (categoryData) {
+      const deleteCategory = await categoryRegister.findOneAndDelete({ categoryName }).lean();
+      if (deleteCategory) {
+        response.onSuccess(res, deleteCategory, 'category is deleted');
+      } else {
+        response.onError(res, 'Record not deleted'); 
+      }
     } else {
-      response.onError(res, 'Record not deleted'); 
+      response.onError(res, 'No data found');
     }
   } else {
-    response.onError(res, 'No data found');
+    response.onError(res, 'CategoryName is required');
   }
 
 };
