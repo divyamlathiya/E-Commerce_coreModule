c1f6c98c73fddd2e809c1fbcc9ebf0d2c1f0f50c
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-05 15:28:46 +0530

    Change delete route from GET to POST method

--- routes/product/delete.js
+++ routes/product/delete.js
@@ -3,10 +3,11 @@ var router = express.Router();
 var deleteProduct = require('../../controller/product/delete.js');
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.post('/', async function(req, res, next) {
 
   await deleteProduct(req, res);
 
 });
 
 module.exports = router;
+
