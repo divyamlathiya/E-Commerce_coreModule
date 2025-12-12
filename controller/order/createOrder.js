1f45d5c464a906675b6e4971e2bd4726bb2e71d2
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-12 16:42:12 +0530

    Add orderItems and paymentDetails

--- controller/order/createOrder.js
+++ controller/order/createOrder.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 var response = require('../../utilities/responseManager.js');
 var orderRegister = require('../../models/order.model.js');
+var orderItemsRegister = require('../../models/orderItem.js');
 var userRegister = require('../../models/userRegister.model.js');
 var cartRegister = require('../../models/cart.model.js');
+var paymentRegister = require('../../models/payment.model.js');
 
 /* GET home page. */
 async function createOrder(req, res, next) {
@@ -11,13 +13,16 @@ async function createOrder(req, res, next) {
     const { paymentMethod, upiId } = req.body;
 
     const foundUser = await userRegister.findById(req.user.userId);
+    const paymentData = await orderRegister.find({ userId: foundUser._id });
 
     if (foundUser) {
-        const foundCart = await cartRegister.findOne({ userId: foundUser._id });
-        if (foundCart && foundCart.items.length > 0) {
+        const foundCart = await cartRegister.find({ userId: foundUser._id });
+        console.log('cart', foundCart)
+        if (foundCart && foundCart.length > 0) {
             const shippingAddress = foundUser.address;
             if (shippingAddress) {
                 if (paymentMethod && ['COD', 'Online'].includes(paymentMethod)) {
+                    const totalAmount = foundCart.reduce((sum, item) => sum + (item.quantity * item.price), 0);
                     if (paymentMethod === 'Online') {
                         if (upiId) {
                             const upiIdRegex = /^[\w.-]+@[\w.-]+$/;
@@ -25,57 +30,104 @@ async function createOrder(req, res, next) {
                                 const newOrder = new orderRegister({
                                     userId: foundUser._id,
                                     userName: foundUser.name,
-                                    email: foundUser.email,
-                                    phone: foundUser.phone,
                                     address: shippingAddress,
-                                    items: foundCart.items.map(item => ({
-                                        productId: item.productId,
-                                        productName: item.name,
-                                        quantity: item.quantity,
-                                        price: item.price
-                                    })),
-                                    totalAmount: foundCart.bill,
+                                    // items: foundCart.map(item => ({
+                                    //     productId: item.productId,
+                                    //     productName: item.name,
+                                    //     quantity: item.quantity,
+                                    //     price: item.price
+                                    //  })),
+                                    totalAmount: totalAmount,
                                     paymentMethod: paymentMethod,
+                                    paymentStatus: 'Pending',
                                     upiId: paymentMethod === "Online" ? upiId : null,
-                                    status: 'Pending'
+                                    orderStatus: 'Pending'
                                 });
 
                                 const saveOrder = await newOrder.save();
 
-                                foundCart.items = [];
-                                foundCart.bill = 0;
-                                await foundCart.save();
+                                const orderItems = foundCart.map(item => ({
+                                    orderId: saveOrder._id,
+                                    userName: item.userName,
+                                    productId: item.productId,
+                                    productName: item.name,
+                                    quantity: item.quantity,
+                                    price: item.price
+                                }));
+
+                                await orderItemsRegister.insertMany(orderItems);
+
+                                const paymentDetails = {
+                                    orderId: saveOrder._id,
+                                    userId: foundUser._id,
+                                    userName: foundUser.name,
+                                    paymentMethod: paymentMethod,
+                                    paymentStatus: 'pending',
+                                    paymentGateway: upiId,
+                                    amount: totalAmount
+                                };
+
+                                await paymentRegister.create(paymentDetails);
+
+                                // foundCart.items = [];
+                                // foundCart.bill = 0;
+
+                                // await foundCart.save();
+                                await cartRegister.deleteMany({ userId: foundUser._id});
 
                                 response.onSuccess(res, saveOrder, 'Order placed successfully');
                             } else {
                                 response.onError(res, 'UpiId is not valid');
                             }
                         } else {
-                            response.onError(res, 'UpiId field is required');
+                            response.onError(res, 'For online payment upiId field is required');
                         }
                     } else {
                         const newOrder = new orderRegister({
                             userId: foundUser._id,
                             userName: foundUser.name,
-                            email: foundUser.email,
-                            phone: foundUser.phone,
                             address: shippingAddress,
-                            items: foundCart.items.map(item => ({
-                                productId: item.productId,
-                                productName: item.name,
-                                quantity: item.quantity,
-                                price: item.price
-                            })),
-                            totalAmount: foundCart.bill,
+                            // items: foundCart.map(item => ({
+                            //     productId: item.productId,
+                            //     productName: item.name,
+                            //     quantity: item.quantity,
+                            //     price: item.price
+                            // })),
+                            totalAmount: totalAmount,
                             paymentMethod: paymentMethod,
-                            status: 'Pending'
+                            paymentStatus: 'Pending',
+                            orderStatus: 'Pending'
                         });
 
                         const saveOrder = await newOrder.save();
 
-                        foundCart.items = [];
-                        foundCart.bill = 0;
-                        await foundCart.save();
+                        const orderItems = foundCart.map(item => ({
+                            orderId: saveOrder._id,
+                            userName: item.userName,
+                            productId: item.productId,
+                            productName: item.name,
+                            quantity: item.quantity,
+                            price: item.price
+                        }));
+
+                        await orderItemsRegister.insertMany(orderItems);
+
+                        const paymentDetails = {
+                            orderId: saveOrder._id,
+                            userId: foundUser._id,
+                            userName: foundUser.name,
+                            paymentMethod: paymentMethod,
+                            paymentStatus: 'Pending',
+                            paymentGateway: upiId,
+                            amount: totalAmount
+                        };
+
+                        await paymentRegister.create(paymentDetails);
+
+                        // foundCart.items = [];
+                        // foundCart.bill = 0;
+                        // await foundCart.save();
+                        await cartRegister.deleteMany({ userId: foundUser._id});
 
                         response.onSuccess(res, saveOrder, 'Order placed successfully');
                     }
