53fd5a39f18091e9bb37c762cfeaf56ad8ce6f8b
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-12 16:40:47 +0530

    Refactor addProduct function for cart management

--- controller/cart/add.js
+++ controller/cart/add.js
@@ -10,56 +10,34 @@ async function addProduct(req, res) {
 
   const { userName, productName, quantity } = req.body;
 
+  const qty = Number(quantity);
+
   if (userName || productName) {
-    const foundUser = await userRegister.findOne({ name: userName });
+    const foundUser = await userRegister.findById(req.user.userId);
     if (foundUser) {
       const checkLoginUser = await userRegister.findById(req.user.userId);
       if (checkLoginUser) {
       const productData = await productAdd.findOne({ name: productName });
       if (productData) {
-        const cartData = await cartAdd.findOne({ userId: foundUser._id });
+        const cartData = await cartAdd.findOne({ userId: foundUser._id, productId: productData._id });
         if (cartData) {
-          const itemIndex = cartData.items.findIndex(item => item.productId.toString() === productData._id.toString());
+          cartData.quantity += qty;
+          cartData.bill = cartData.quantity * productData.price;
 
-          if (itemIndex > -1) {
-            let productItem = cartData.items[itemIndex];
-            productItem.quantity += quantity;
-            cartData.items[itemIndex] = productItem;
-          } else {
-            // const product = await productAdd.findOne({ name: productName });
-            // if (!productData) {
-            //   response.onError(res, 'Product not found');
-            // } else {
-            cartData.items.push({
-              productId: productData._id,
-              name: productData.name,
-              quantity,
-              price: productData.price
-            });
-            // }
-          }
-          cartData.bill = cartData.items.reduce((acc, curr) => acc + curr.quantity * curr.price, 0);
           const cartSave = await cartData.save();
           response.onSuccess(res, cartSave, 'Product added');
         } else {
-          // const product = await productAdd.findById(productId);
-          if (!productData) {
-            response.onError(res, 'Product not found');
-          } else {
             const newCart = new cartAdd({
               userId: foundUser._id,
               userName: foundUser.name,
-              items: [{
-                productId: productData._id,
-                name: productData.name,
-                quantity,
-                price: productData.price
-              }],
-              bill: quantity * productData.price
+              productId: productData._id,
+              name: productData.name,
+              quantity: qty,
+              price: productData.price,
+              bill: qty * productData.price
             });
             await newCart.save();
             response.onSuccess(res, newCart, 'Product added');
-          }
         }
       } else {
         response.onError(res, 'Product not found');
