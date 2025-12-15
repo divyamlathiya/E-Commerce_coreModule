09c4d7c2bc5e9376ddc5401d6e148f2a34890d2d
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-15 16:07:20 +0530

    Enhance password encryption with multi-layer approach

--- utilities/EncDec.js
+++ utilities/EncDec.js
@@ -1,14 +1,20 @@
 const crypto = require('crypto-js');
 require('dotenv').config();
 
+const secrt_key = process.env.CRYPTO_SECRET;
+
 var encPass = async (password) => {
-    var encPass = await crypto.AES.encrypt(password, process.env.CRYPTO_SECRET);
-    return encPass.toString();
+    var encPassLayer1 = await crypto.AES.encrypt(password, secrt_key).toString();
+    var encPassLayer2 = await crypto.DES.encrypt(encPassLayer1, secrt_key).toString();
+    var encPassLayer3 = await crypto.TripleDES.encrypt(encPassLayer2, secrt_key).toString();
+    return encPassLayer3;
 };
 
 var decPass = async (password) => {
-    var decPass = await crypto.AES.decrypt(password, process.env.CRYPTO_SECRET);
-    return decPass.toString(crypto.enc.Utf8);
+    var decPassLayer3 = await crypto.TripleDES.decrypt(password, secrt_key).toString(crypto.enc.Utf8);
+    var decPassLayer2 = await crypto.DES.decrypt(decPassLayer3, secrt_key).toString(crypto.enc.Utf8);
+    var decPassLayer1 = await crypto.AES.decrypt(decPassLayer2, secrt_key).toString(crypto.enc.Utf8);
+    return decPassLayer1;
 };
 
 module.exports = { encPass, decPass };
