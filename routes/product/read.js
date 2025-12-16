f476e7d85b44583f7e247a16b9c7f761dd6f6fd2
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-16 15:40:32 +0530

    Update read.js

--- routes/product/read.js
+++ routes/product/read.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var readProduct = require('../../controller/product/read.js');
+var { isAdmin } = require('../../middleware/isAdmin.js');
 
 /* GET home page. */
-router.get('/',async function(req, res, next) {
+router.get('/', isAdmin, async function(req, res, next) {
 
   await readProduct(req, res);
 
