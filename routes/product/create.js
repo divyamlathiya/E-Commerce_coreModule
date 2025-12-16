f91b09843be8cf638ae51e55d0eb59682f1a087a
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-16 15:40:10 +0530

    Update create.js

--- routes/product/create.js
+++ routes/product/create.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var createProduct = require('../../controller/product/create.js');
+var { isAdmin } = require('../../middleware/isAdmin.js');
 
 /* GET users listing. */
-router.post('/', async function (req, res, next) {
+router.post('/', isAdmin, async function (req, res, next) {
   
   await createProduct(req, res);
 
