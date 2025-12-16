c913a7944593d54921321596545766c0116fa8fb
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-16 15:39:36 +0530

    Add isAdmin middleware to category update route

--- routes/category/update.js
+++ routes/category/update.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var updatedCategory = require('../../controller/category/update.js');
+var { isAdmin } = require('../../middleware/isAdmin.js');
 
 /* GET users listing. */
-router.get('/', async function (req, res, next) {
+router.get('/', isAdmin, async function (req, res, next) {
   
   await updatedCategory(req, res);
 
