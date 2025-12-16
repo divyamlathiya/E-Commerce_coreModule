e56d59cc072296d0cfed43d3b5c62bf466223d98
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-16 15:39:23 +0530

    Add isAdmin middleware to category read route

--- routes/category/read.js
+++ routes/category/read.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 var readCategory = require('../../controller/category/read.js');
+var { isAdmin } = require('../../middleware/isAdmin.js');
 
 /* GET users listing. */
-router.get('/', async function (req, res, next) {
+router.get('/', isAdmin, async function (req, res, next) {
   
   await readCategory(req, res);
 
