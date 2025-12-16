e2b00b8d96823b8469dca44af1554602b4abd907
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-16 15:40:21 +0530

    Update delete.js

--- routes/product/delete.js
+++ routes/product/delete.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var deleteProduct = require('../../controller/product/delete.js');
+var { isAdmin } = require('../../middleware/isAdmin.js');
 
 /* GET home page. */
-router.post('/', async function(req, res, next) {
+router.post('/', isAdmin, async function(req, res, next) {
 
   await deleteProduct(req, res);
 
 });
 
 module.exports = router;
-
