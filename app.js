5771033217be9b3d98c2e63d93aa68c0f4a48e59
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-17 15:30:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -69,7 +69,8 @@ orderRoute = [
 ];
 
 adminRoute = [
-  { path: '/adminLogin', routePath: '/adminLogin' }
+  { path: '/adminLogin', routePath: '/adminLogin' },
+  { path: '/getAdminProfl', routePath: '/getAdminProfl' }
 ];
 
 
