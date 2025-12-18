e9327778accc52ac9695078dcaa997e0046ec291
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-18 15:55:03 +0530

    Add transporter and mailOptions to send mail using nodeMailer in reqForResetPass.js

--- controller/user/forgetPassword/reqForResetPass.js
+++ controller/user/forgetPassword/reqForResetPass.js
@@ -4,6 +4,7 @@ var crypto = require('crypto');
 var response = require('../../../utilities/responseManager.js');
 var resetTokens = require('../../../utilities/resetTokens.js');
 var userRegister = require('../../../models/userRegister.model.js');
+var nodeMailer = require('nodemailer');
 
 /* GET home page. */
 async function reqForResetPass(req, res, next) {
@@ -14,10 +15,27 @@ async function reqForResetPass(req, res, next) {
     const foundUser = await userRegister.findOne({ email: email });
     if (foundUser) {
         const token = crypto.randomBytes(4).toString('hex');
-        const expiresAt = Date.now() + 15 * 60 * 1000;
+        const expiresAt = new Date(Date.now() + 15 * 60 * 1000);
 
         resetTokens[email] = { token, expiresAt };
         console.log(`Token for reset password is ${email}: ${token}`);
+
+        const transporter = nodeMailer.createTransport({
+          host: "smtp.ethereal.email",
+          port: 587,
+          secure: false,
+          auth: {
+            user: "emmie53@ethereal.email",
+            pass: process.env.etherRealPass
+          }
+        });
+
+        const mailOption = transporter.sendMail({
+          from: "emmie53@ethereal.email",
+          to: email,
+          subject: "Forget password",
+          text: `Token for reset password is: ${token} and this token expires at ${expiresAt.toLocaleString()}`
+        });
 
         response.onSuccess(res, null, `Token send to ${email}`);
     } else {
