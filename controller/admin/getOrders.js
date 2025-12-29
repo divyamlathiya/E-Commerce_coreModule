f9890b0787e088fad262ca298aa3ebeb9eb860be
Divyam Lathiya <169042757+divyamlathiya@users.noreply.github.com>
2025-12-29 16:00:02 +0530

    Update require function

--- controller/admin/getOrders.js
+++ controller/admin/getOrders.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var response = require('../../utilities/responseManager.js');
-var orderRegister = require('../../models/orderItem.model.js');
+var orderItemRegister = require('../../models/orderItem.model.js');
 
 /* GET home page. */
 async function getOrders(req, res, next) {
 
-    const getOrders = await orderRegister.find({  });
+    const getOrders = await orderItemRegister.find({  });
 
     if (getOrders) {
         response.onSuccess(res, getOrders, 'Order found');
@@ -17,3 +17,4 @@ async function getOrders(req, res, next) {
 };
 
 module.exports = getOrders;
+
